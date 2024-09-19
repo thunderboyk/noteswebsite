9fc4016fb0b3f80525ca2fb2e63074bf9709fb1e
thunderboyk <lakshyajun27@gmail.com>
2024-09-19 17:19:48 +0530

    Add files via upload

--- toodoo/todo/views.py
+++ toodoo/todo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+#this a dgango form for signup page 
 
 # Create your views here.
 def signupuser(request):
-	return render(request,'todo/signupuser.html')
+	
+	return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+	#creating a dictonary and giving a key to the form 
